1af12986df22a8db127674bbfe743a3fcab0e637
Anish Chadalavada <anish.chada@gmail.com>
2017-04-13 13:32:56 -0400

    final

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ import './style.scss';
 
 // Referenced https://www.w3schools.com/jsref/met_win_setinterval.asp
 // Referenced https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_win_setinterval2
-
 // Maybe make t a local variable and use let, and take var myVar out because its not necessary?x
 
 let t = 0;
